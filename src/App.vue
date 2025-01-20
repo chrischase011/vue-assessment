3e17c6065dff3c9f6ba5f1a48b0c7bca790d3da9
Christopher Robin Chase <christopherchase011@gmail.com>
2025-01-20 15:01:42 +0800

    Added Notification Banner

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import TaskContainer from './components/TaskContainer.vue'
+import NotificationBanner from './components/NotificationBanner.vue'
 </script>
 
 <template>
   <div class="container mx-auto flex flex-col items-center justify-center h-screen my-7 gap-5">
     <h1 class="text-4xl font-bold">Vue 3</h1>
+    <NotificationBanner message="Hello world! This is a Notification Banner" />
     <TaskContainer /> 
   </div>
 
---
+++ src/components/NotificationBanner.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    v-if="visible"
+    class="relative w-full max-w-[450px] flex items-center gap-3 p-3 bg-emerald-500 border border-emerald-700 text-white rounded-md shadow-md"
+  >
+    <div>
+        <img :src="infoSVG" class="w-[20px] h-[20px] fill-white" alt="">
+    </div>
+    <div class="flex-1">
+      {{ message }}
+    </div>
+    <button
+      @click="closeBanner"
+      class="absolute top-0 right-3 bg-transparent border-none text-white text-lg font-bold hover:opacity-80 focus:outline-none p-0 cursor-pointer"
+      aria-label="Close notification"
+    >
+      &times;
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import infoSVG from '../assets/info.svg';
+// Props
+const props = defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+});
+
+const visible = ref(true);
+
+const closeBanner = () => {
+  visible.value = false;
+};
+</script>
+
+<style scoped>
+</style>
